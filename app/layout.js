5c0599a85a8eb7e1f4c32c2d2b89d2a00e9648ea
devDhiraj12 <dhirajdagabaj0412@gmail.com>
2025-02-27 15:04:40 +0530

    google analytics

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,8 @@ import Navbar from './components/navbar';
 import './css/card.scss';
 import './css/globals.scss';
 const inter = Inter({ subsets: ['latin'] });
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 export const metadata = {
   title: 'devDhiraj12 | DevOps Engineer',
@@ -21,6 +23,7 @@ export default function RootLayout({ children }) {
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
+          <GoogleAnalytics gaId="G-SBCN1MCNZ0" />
         </main>
         <Footer />
       </body>
